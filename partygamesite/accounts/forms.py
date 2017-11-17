0552a823581d85f31f608fb62d8a3a5359b57d2a
Jonathan Berg Hjelm <jbh@SSI-NE.local>
2017-11-17 15:15:43 +0100

    Nothing works fix!

---
+++ partygamesite/accounts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Enter username',max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
--- partygamesite/accounts/urls.py
+++ partygamesite/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from django.contrib.auth import views
 
 app_name = 'accounts'
 
